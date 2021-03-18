670463c46f92201b3d51c76af13da1796021464d
Fabricio Bianchi <fabriciob91@gmail.com>
2021-03-17 23:32:04 -0300

    commit inicial, archivos desde sourceforge con librerias generadas

---
+++ symfony/web/resource_dir_inc.php
@@ -0,0 +1,5 @@
+<?php 
+sfConfig::set('sf_web_css_dir_name','webres_5fee89a90600f2.94309009/css');
+sfConfig::set('sf_web_js_dir_name','webres_5fee89a90600f2.94309009/js');
+sfConfig::set('sf_web_images_dir_name','webres_5fee89a90600f2.94309009/images');
+sfConfig::set('ohrm_resource_dir', 'webres_5fee89a90600f2.94309009');
